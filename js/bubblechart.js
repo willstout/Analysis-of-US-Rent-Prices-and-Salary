d033fdd22de9e5710a37fb47f0740a882a347dbb
xuyang-li <xuyang.li@hotmail.com>
2020-11-15 18:15:31 -0700

    Modified tick labels

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -19,14 +19,14 @@ class BubbleChart {
             .attr('height', 770)
             .attr('rx', 50)
             .attr('ry', 50)
-            .attr('transform', 'translate(60, 0)');
+            .attr('transform', 'translate(90, 0)');
         
         let svgGroup = d3.select('#bubbleChart').select('svg')
             .append('g')
             .attr('id', 'plotGroup')
             .attr('width', '100%')
             .attr('height', '100%')
-            .attr('transform', 'translate(110, 10)');
+            .attr('transform', 'translate(140, 10)');
 
         svgGroup.append('g').attr('id', 'axis');
         svgGroup.append('g').attr('id', 'bubbleGroup');
@@ -47,7 +47,7 @@ class BubbleChart {
                         .range([40, 710]);
 
         let ticks = [sMax, sMin];
-        let tickValue = sMax - sMax%10000;
+        let tickValue = sMax - sMax%5000;
         while (tickValue > sMin) {
             ticks.push(tickValue);
             tickValue -= 5000;
@@ -55,7 +55,7 @@ class BubbleChart {
 
         d3.select("#axis").call(d3.axisLeft(this.axisScale)
                                     .tickPadding(20)
-                                    .tickFormat(d3.format(".2s"))
+                                    //.tickFormat(d3.format(".2s"))
                                     .tickSize(100)
                                     .tickValues(ticks))
                         .attr('transform', 'translate(50, 0)');
